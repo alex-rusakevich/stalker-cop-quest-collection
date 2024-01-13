d56ed99df0a54ec20efc48a2ecc765edbfd97231
Alexander Rusakevich <mr.alexander.rusakevich@gmail.com>
2024-01-14 00:13:51 +0300

    #patch first commit

---
+++ choose-stalker-folder.ps1
@@ -0,0 +1,13 @@
+[void] [Reflection.Assembly]::LoadWithPartialName( 'System.Windows.Forms' )
+
+$open_folder_dialog = New-Object Windows.Forms.FolderBrowserDialog
+$open_folder_dialog.rootfolder = "MyComputer"
+$open_folder_dialog.Description = 
+    "Выберите корневую папку игры (в ней должна быть папка 'bin', но саму папку 'bin' не выбирайте)"
+$folder = ""
+
+if($open_folder_dialog.ShowDialog() -eq "OK") {
+    $folder = $open_folder_dialog.SelectedPath
+}
+
+$folder > .stalkerpath
